6feb79f5d9dc74e412bf1208feee11c5aaf71b06
HolyRead <abhay@holyreads.com>
2021-12-27 15:40:49 +0530

    Feat admin signin with otp

--- src/routes/admin/auth.route.ts
+++ src/routes/admin/auth.route.ts
@@ -3,5 +3,6 @@ import authService from '../../controllers/admin/auth.controller'
 const router: Router = express.Router()
 
 router.post('/login', authService.signInUser)
+router.post('/login/verify', authService.verifySignInOtp)
 
 export default router

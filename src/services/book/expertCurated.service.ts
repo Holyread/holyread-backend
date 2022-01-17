8ccf5f7e9e2bd611e24c8c4936093c9162d88917
Holyread <abhay@holyreads.com>
2022-01-17 18:00:31 +0530

    update image path

--- src/services/book/expertCurated.service.ts
+++ src/services/book/expertCurated.service.ts
@@ -53,6 +53,13 @@ const getAllExpertCurated = async (skip: number, limit, search: object, sort) =>
     try {
         const result = await ExpertCuratedModel.find(search).skip(skip).limit(limit).sort(sort).lean()
         const count = await ExpertCuratedModel.find(search).count()
+        if (result.length) {
+            await Promise.all(result.map(item => {
+                if (item && item.image) {
+                    item.image = awsBucket[NODE_ENV].s3BaseURL + '/' + awsBucket.bookDirectory + '/expertCurated/' + item.image
+                }
+            }))
+        }
         return { count, data: result }
     } catch (e: any) {
         throw new Error(e)

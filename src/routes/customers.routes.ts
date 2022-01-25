22abbf4200f20c563321d44c7afe44a584854334
Holyread <abhay@holyreads.com>
2022-01-25 15:37:11 +0530

    Enable cors

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -7,7 +7,7 @@ import { corsOptionsDelegate } from '../app'
 
 const router: Router = express.Router()
 
-router.use('/auth', auth)
+router.use('/auth', cors(corsOptionsDelegate), auth)
 router.use('/users', cors(corsOptionsDelegate), users)
 
 export default router

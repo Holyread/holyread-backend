b058a8ee2c0cd753e99a189daad5b1d2e2bbf9ac
Holyread <abhay@holyreads.com>
2022-02-07 18:46:06 +0530

    Feat customer books apis

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -3,11 +3,13 @@ import express, { Router } from 'express'
 import auth from './customers/auth.route'
 import users from './customers/users.route'
 import dashboard from './customers/dashboard.route'
+import bookSummary from './customers/book/bookSummary.route'
 
 const router: Router = express.Router()
 
 router.use('/auth', auth)
 router.use('/users', users)
 router.use('/dashboard', dashboard)
+router.use('/book-summary', bookSummary)
 
 export default router

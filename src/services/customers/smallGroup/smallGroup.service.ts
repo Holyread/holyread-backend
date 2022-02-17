f1237a781337350614c6fddd1b518fc1b2877eb3
Holyread <abhay@holyreads.com>
2022-02-17 15:35:01 +0530

    Small group images and pagination support for discover page

--- src/services/customers/smallGroup/smallGroup.service.ts
+++ src/services/customers/smallGroup/smallGroup.service.ts
@@ -1,10 +1,24 @@
-import { SmallGroupModel } from '../../../models/index'
+import { SmallGroupModel, BookSummaryModel } from '../../../models/index'
+import { awsBucket } from '../../../constants/app.constant'
+import config from '../../../../config'
+
+const NODE_ENV = config.NODE_ENV
 
 /** Get all small group for app */
 const getAllSmallGroups = async (skip: number, limit, search: object, sort) => {
     try {
         let result: any = await SmallGroupModel.find(search).skip(skip).limit(limit).sort(sort).lean()
-        result = result.map(item => {
+        result = await Promise.all(await result.map(async (item) => {
+            if (item && item.books && item.books.length) {
+                item.books = await Promise.all(item.books.map(async oneBook => {
+                    const bookDetails = await BookSummaryModel.findById(oneBook).select('coverImage').lean().exec()
+                    if (!bookDetails) {
+                        return oneBook
+                    }
+                    bookDetails.coverImage = awsBucket[NODE_ENV].s3BaseURL + '/' + awsBucket.bookDirectory + '/coverImage/' + bookDetails.coverImage
+                    return bookDetails
+                }))
+            }
             return {
                 _id: item._id,
                 iceBreaker: item.iceBreaker,
@@ -12,8 +26,9 @@ const getAllSmallGroups = async (skip: number, limit, search: object, sort) => {
                 title: item.title,
                 description: item.description,
                 backgroundColor: item.backgroundColor,
+                books: item.books
             }
-        })
+        }))
         return result
     } catch (e: any) {
         throw new Error(e)

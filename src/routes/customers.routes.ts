cc50481bef548b31107c46e443fc43a35875aec4
Holyread <abhay@holyreads.com>
2022-03-21 23:13:27 +0530

    Feat customer book apis

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -5,6 +5,7 @@ import users from './customers/users.route'
 import dashboard from './customers/dashboard.route'
 import bookSummary from './customers/book/bookSummary.route'
 import highLight from './customers/highLight.route'
+import expertCurated from './admin/book/expertCurated.route'
 
 const router: Router = express.Router()
 
@@ -13,5 +14,6 @@ router.use('/users', users)
 router.use('/dashboard', dashboard)
 router.use('/book-summary', bookSummary)
 router.use('/high-lights', highLight)
+router.use('/expert-curated', expertCurated)
 
 export default router

38717255e610d32cbed4e1710e25d46b7aa3b4d6
Holyread <abhay@holyreads.com>
2022-05-05 17:52:15 +0530

    Fix search book on discover page

--- src/services/customers/book/bookSummary.service.ts
+++ src/services/customers/book/bookSummary.service.ts
@@ -5,8 +5,46 @@ import { randomNumberInRange } from '../../../lib/utils/utils'
 
 const NODE_ENV = config.NODE_ENV
 
-/** Get all book summaries */
-const getAllBookSummaries = async (skip: number, limit, search: object, sort, library?: any) => {
+/** Get all book summaries with filter by author id or author name, book title or all */
+const getAllBookSummariesForDiscover = async (skip: number, limit, search: any, sort, library?: any) => {
+    try {
+        let result: any = await BookSummaryModel.find({}).sort(sort).lean().exec()
+        // let count: any = await BookSummaryModel.count(search).lean().exec()
+        result = await Promise.all(result.map(async oneItem => {
+            if (oneItem.author) {
+                oneItem.author = await BookAuthorModel.findById(oneItem.author).lean()
+                oneItem.author = {
+                    _id: oneItem.author._id,
+                    name: oneItem.author.name
+                }
+            }
+            /** if search author then return if book author not match */
+            if (search?.author && String(oneItem.author) !== search?.author) return null
+            /** if search then return if book title or author name not match */
+            if (search.filter && !oneItem?.title?.toLowerCase()?.includes(search.filter) && !oneItem?.author?.name?.toLowerCase().includes(search.filter)) return null
+
+            const isSaved = global?.currentUser?.library?.saved?.find(b => String(b) === String(oneItem?._id)) ? true : false
+            return {
+                _id: oneItem._id,
+                coverImage: awsBucket[NODE_ENV].s3BaseURL + '/' + awsBucket.bookDirectory + '/coverImage/' + oneItem.coverImage,
+                title: oneItem.title,
+                author: oneItem.author,
+                overview: oneItem.overview,
+                totalStar: Number(randomNumberInRange(3, 4) + '.' + (randomNumberInRange(1, 9))),
+                totalReads: randomNumberInRange(10000, 20000),
+                bookMark: isSaved,
+                coverImageBackground: oneItem.coverImageBackground,
+            }
+        }))
+        result = result.filter(s => s)
+        return { count: result.length, summaries: result.slice(skip, skip + limit) }
+    } catch (e: any) {
+        throw new Error(e)
+    }
+}
+
+/** Get user library book summaries */
+const getAllBookSummaries = async (skip: number, limit, search: any, sort, library?: any) => {
     try {
         let result: any = await BookSummaryModel.find(search).skip(skip).limit(limit).sort(sort).lean().exec()
         let count: any = await BookSummaryModel.count(search).lean().exec()
@@ -59,6 +97,7 @@ const getOneBookSummaryByFilter = async (query: any) => {
 
 export default {
     getAllBookSummaries,
+    getAllBookSummariesForDiscover,
     getOneBookSummaryByFilter
 }
 

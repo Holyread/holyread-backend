1d7a68afd852f4241d44bee94006d0068ebcad02
Holyread <abhay@holyreads.com>
2022-05-06 16:20:03 +0530

    Fix Regex

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -169,7 +169,10 @@ export const sentEmail = async (receiverEmail: string, subject: string, html: st
 
 export const getSearchRegexp = async (value) => {
     if (value.toString().startsWith('+')) { return value.slice(1) }
-    const result = { $regex: '.*' + value.trim() + '.*', $options: '-i' }
+    if (value.toString().startsWith('???')) { value = value.replace('???', '?') }
+    if (value.toString().startsWith('[')) { value = value.slice(1) }
+    if (value.toString().startsWith('(')) { value = value.slice(1) }
+    const result = { $regex: `.*\\$aus.*` + value.trim() + '.*\\$aus.*', $options: '-i' }
     return result
 }
 

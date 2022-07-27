a88225acddacb4cf9bae9ec601ff264d4069eb15
Holyread <abhay@holyreads.com>
2022-07-27 18:13:45 +0530

    Fix small issue

--- src/controllers/admin/book/bookSummary.controller.ts
+++ src/controllers/admin/book/bookSummary.controller.ts
@@ -114,11 +114,6 @@ const getAllSummaries = async (request: Request, response: Response, next: NextF
                 $or: [
                     { 'title': await getSearchRegexp(params.search) },
                     { 'status': await getSearchRegexp(params.search) },
-                    { 'description': await getSearchRegexp(params.search) },
-                    { 'overview': await getSearchRegexp(params.search) },
-                    { 'bookFor': await getSearchRegexp(params.search) },
-                    { 'chapters.name': await getSearchRegexp(params.search) },
-                    { 'author.name': await getSearchRegexp(params.search) }
                 ]
             }
         }

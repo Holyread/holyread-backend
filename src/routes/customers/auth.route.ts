2c1fea4033abb17ce4c17e45104c56d90ff617d5
Holyread <abhay@holyreads.com>
2022-08-23 18:18:17 +0530

    Feat routes

--- src/routes/customers/auth.route.ts
+++ src/routes/customers/auth.route.ts
@@ -6,6 +6,8 @@ router.post('/signUp', authController.signUpUser)
 router.get('/signUp/verify', authController.verifyUserSignUp)
 router.post('/login', authController.signInUser)
 router.post('/oauth-login', authController.oAuthLogin)
+router.post('/oauth-signin/app', authController.appOAuthSignIn)
+router.post('/oauth-signup/app', authController.appOAuthSignUp)
 router.post('/forgot-password', authController.forgotPassoword)
 router.post('/verify-password', authController.verifyPassword)
 

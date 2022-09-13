864f318ecec60206130a95c0acb55d363ec9223b
Holyread <abhay@holyreads.com>
2022-09-13 03:09:34 -0300

    Feat referral user name and email

--- src/middleware/customers.passport.ts
+++ src/middleware/customers.passport.ts
@@ -18,6 +18,8 @@ export default async (req: any, res: Response, next: NextFunction): Promise<any>
             if (userDetails.status !== 'Active') {
                 next(Boom.badRequest('User not active'));
             }
+            const refUser: any = await UserModel.findOne({ _id: userDetails.referralUserId }).select('firstName lastName email').lean().exec()
+            if (refUser) userDetails.referralUserId = refUser
             req.user = { ...userDetails, isNewLogin: eval(details?.isNewLogin) }
             global.currentUser = req.user;
             if (req?.user?.stripe?.subscriptionId) {

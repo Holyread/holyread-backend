5fcbf31e3472e1d691e83ced94c3505d23d63882
Holyread <abhay@holyreads.com>
2022-12-26 21:08:35 +0530

    Small improvments

--- src/scripts/fixBookFilesName.ts
+++ src/scripts/fixBookFilesName.ts
@@ -18,11 +18,13 @@ import { awsBucket } from '../constants/app.constant';
                   name: string,
                   newKey: string
             ) => {
-                  const bucketName: string = awsBucket[config.NODE_ENV].bucketName
+                  const bucketName: string
+                        = awsBucket[config.NODE_ENV].bucketName
                   
                   return await copyS3File({
                         bucketName,
-                        oldKey: `${bucketName}/${awsBucket.bookDirectory}/audio/${name}`,
+                        oldKey:
+                              `${bucketName}/${awsBucket.bookDirectory}/audio/${name}`,
                         newKey
                   });
             }
@@ -41,22 +43,34 @@ import { awsBucket } from '../constants/app.constant';
 
                   const newKey: string
                         = `${awsBucket.bookDirectory}/audio/${fileName}${format}`;
-
+                  
+                  let isUpdate: boolean = false;
                   if (name !== `${fileName}${format}`) {
                         await copyFile(name, newKey);
                         name = `${fileName}${format}`
+                        isUpdate = true;
                   } else {
                         name = original
                   }
-                  return name
+                  return { name, isUpdate }
             }
 
+            let vc = 0, ac = 0;
             await Promise.all(books.map(async (book) => {
                   try {
                         if (book.videoFile) {
                               try {
-                                    book.video = book.videoFile
-                                    book.videoFile = await next(book.videoFile)
+                                    const {
+                                          name,
+                                          isUpdate
+                                    } = await next(
+                                          book.videoFile
+                                    ) 
+                                    if (isUpdate) {
+                                          book.video = book.videoFile
+                                          vc++;
+                                    }
+                                    book.videoFile = name
                               } catch ({ message }) {
                                     console.log(
                                           'Book video processing error for book ',
@@ -68,11 +82,17 @@ import { awsBucket } from '../constants/app.constant';
                               book.chapters.map(async (chapter) => {
                                     try {
                                           if (chapter.audioFile) {
-                                                chapter.audio = chapter.audioFile
-                                                chapter.audioFile
-                                                      = await next(
-                                                            chapter.audioFile
-                                                      )
+                                                const {
+                                                      name,
+                                                      isUpdate
+                                                } = await next(
+                                                      book.videoFile
+                                                ) 
+                                                if (isUpdate) {
+                                                      chapter.audio = chapter.audioFile
+                                                      ac++;
+                                                }
+                                                chapter.audioFile = name
                                           }
                                     } catch ({ message }) {
                                           console.log(
@@ -100,10 +120,16 @@ import { awsBucket } from '../constants/app.constant';
                         )
                   }
             }));
-            console.log('Book files name fixed successfully')
+            console.log(
+                  'Book files name fixed successfully',
+                  'Total video updated:', vc,
+                  'Total audio updated:', ac,
+            )
 
       } catch (e: any) {
-            console.log('Fix books files name script execution failed - ', e)
+            console.log(
+                  'Fix books files name script execution failed - ',
+                  e
+            )
       }
-      // return true;
 })();

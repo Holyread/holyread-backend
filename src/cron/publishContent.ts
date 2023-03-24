772a5a73b0e6aa0b62ed858101aff4f6fa77d549
Patoliya <developer.patoliya@gmail.com>
2023-03-24 18:05:40 +0530

    add random user code if user not found

--- src/cron/publishContent.ts
+++ src/cron/publishContent.ts
@@ -17,89 +17,98 @@ const start = async () => {
 
             /** Publish first in  */
             if (unpublishBooks.length && unpublishBooks[0]?._id) {
-                  await BookSummaryModel.findOneAndUpdate({ _id: unpublishBooks[0]?._id }, { publish: true })
+                  await BookSummaryModel.findOneAndUpdate({ _id: unpublishBooks[0]?._id }, { publish: true });
 
-                  const newPubishBook = await BookSummaryModel.findOne(unpublishBooks[0]._id).populate('author').lean().exec();
+                  /** Get New Pubish Book by id */
+                  const newPubishBook = await BookSummaryModel.findOne(unpublishBooks[0]._id)
+                        .select([
+                              '_id',
+                              'description',
+                              'overview',
+                              'bookFor',
+                              'categories',
+                              'coverImageBackground',
+                              'title',
+                              'author',
+                              'views',
+                              'coverImage',
+                              'totalStar',
+                              'status'
+                        ]).populate('author').lean().exec();
 
                   /** Initialize default ratings for books by machine user */
+                  try {
+                        /** Find bot user */
+                        var botUser: any = await UserModel.findOne({
+                              email: 'bot@holyreads.com'
+                        })
+                              .select('_id')
+                              .lean()
+                              .exec()
 
-                  (async () => {
-                        try {
-                              /** Find bot user */
-                              const botUser = await UserModel.findOne({
-                                    email: 'bot@holyreads.com'
-                              })
-                                    .select('_id')
-                                    .lean()
-                                    .exec()
-
-                              if (!botUser) {
-                                    console.log(
-                                          'Bot user does not exit'
-                                    );
-                                    return false;
-                              }
-
-                              const star = Number(
-                                    `${randomNumberInRange(4, 5)}.${randomNumberInRange(1, 5)}`
-                              )
-
-                              await RatingModel.findOneAndUpdate(
-                                    { userId: botUser._id, bookId: newPubishBook._id },
-                                    { star, updatedAt: new Date() },
-                                    { upsert: true }
-                              )
-                              console.log('Default Ratings added successfully');
-
-                              let totalRating = await RatingModel.findOne({ userId: botUser._id, bookId: newPubishBook._id }).select('star').lean().exec();
-                              const rating = totalRating.star
-                              const publishContent = { ...newPubishBook, rating }
-
-                              const users = await UserModel.find({
-                                    status: 'Active',
-                                    timeZone: { $exists: true },
-                                    'pushTokens.0': { '$exists': true },
-                                    'notification.push': true,
-                              }).select('timeZone pushTokens').lean().exec()
-
-                              if (!users.length) {
-                                    console.log('JOB(🔴) publish contents execution stop due to no users found');
-                              }
-                              users.map(i => {
-                                    const tokenSet = new Set();
-                                    tokenSet.add(
-                                          pushNotification(
-                                                i?.pushTokens?.map((ti: { token: string }) => ti.token) || [],
-                                                '🔔 NEW Publish book for you',
-                                                `📙 ${unpublishBooks[0]?.title}`,
-                                                JSON.stringify({
-                                                      publishContents: {
-                                                            _id: publishContent._id,
-                                                            description: publishContent.description,
-                                                            overview: publishContent.overview,
-                                                            bookFor: publishContent.bookFor,
-                                                            categories: publishContent.categories,
-                                                            coverImageBackground: publishContent.coverImageBackground,
-                                                            title: publishContent.title,
-                                                            author: publishContent.author,
-                                                            views: publishContent.views,
-                                                            publish: publishContent.publish,
-                                                            coverImage: awsBucket[config.NODE_ENV].s3BaseURL + '/' + awsBucket.bookDirectory + '/coverImage/' + publishContent.coverImage,
-                                                            totalStar: publishContent.rating,
-                                                            status: publishContent.status,
-                                                      }
-                                                })
-                                          ).catch(() => { return undefined; })
-                                    )
-                              })
-                        } catch ({ message }: any) {
-                              console.log(
-                                    'Add default ratings script execution failed: Error: ',
-                                    message
-                              )
+                        /**Find Random User */
+                        if (!botUser) {
+                              var botUser: any = await UserModel.aggregate([{ $sample: { size: 1 } }])
                         }
-                        return true;
-                  })();
+
+                        /** Add Default ratingin New Publish Book */
+                        const star = Number(
+                              `${randomNumberInRange(4, 5)}.${randomNumberInRange(1, 5)}`
+                        )
+
+                        await RatingModel.findOneAndUpdate(
+                              { userId: botUser._id, bookId: newPubishBook._id },
+                              { star, updatedAt: new Date() },
+                              { upsert: true }
+                        )
+
+                        /** Get book rating */
+                        const Bookrating = await RatingModel.findOne({ userId: botUser._id, bookId: newPubishBook._id }).select('star').lean().exec();
+                        var publishContent = { ...newPubishBook, Bookrating }
+                  } catch ({ message }: any) {
+                        console.log(
+                              'Add default ratings  execution failed: Error: ',
+                              message
+                        )
+                  }
+
+                  /** Find Users and push notification of New Publish Book*/
+                  const users = await UserModel.find({
+                        status: 'Active',
+                        timeZone: { $exists: true },
+                        'pushTokens.0': { '$exists': true },
+                        'notification.push': true,
+                  }).select('timeZone pushTokens').lean().exec()
+
+                  if (!users.length) {
+                        console.log('JOB(🔴) publish contents execution stop due to no users found');
+                  }
+                  users.map(i => {
+                        const tokenSet = new Set();
+                        tokenSet.add(
+                              pushNotification(
+                                    i?.pushTokens?.map((ti: { token: string }) => ti.token) || [],
+                                    '🔔 NEW Publish book for you',
+                                    `📙 ${unpublishBooks[0]?.title}`,
+                                    JSON.stringify({
+                                          publishContents: {
+                                                _id: publishContent._id,
+                                                description: publishContent.description,
+                                                overview: publishContent.overview,
+                                                bookFor: publishContent.bookFor,
+                                                categories: publishContent.categories,
+                                                coverImageBackground: publishContent.coverImageBackground,
+                                                title: publishContent.title,
+                                                author: publishContent.author,
+                                                views: publishContent.views,
+                                                coverImage: awsBucket[config.NODE_ENV].s3BaseURL + '/' + awsBucket.bookDirectory + '/coverImage/' + publishContent.coverImage,
+                                                totalStar: publishContent.Bookrating.star,
+                                                status: publishContent.status,
+                                          }
+                                    })
+                              ).catch(() => { return undefined; })
+                        )
+                  })
             }
 
             if (unpublishCurateds.length && unpublishCurateds[0]?._id) {

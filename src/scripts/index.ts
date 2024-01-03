9bac8647ad0cbe440fe00933a804d11ddc1b32e7
Patoliya <developer.patoliya@gmail.com>
2024-01-03 09:55:24 +0530

    feat-disable not verified user script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,3 +3,4 @@ import './set-default-views.script';
 import './set-stripe-coupon-and-status';
 import './set-expiry-date';
 import './set-last-transaction-id';
+// import './set-verified-and-activate-users.script'                                                                                               

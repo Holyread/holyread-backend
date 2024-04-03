a74e9374f3724cc238f6d229adeb9a20df648990
Patoliya <developer.patoliya@gmail.com>
2024-04-03 12:14:11 +0530

    feat: Daily Devotional Unlimited access to all past and present daily devotionals.

---
+++ src/controllers/customers/book/dailyDevotional.controller.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from 'express'
+import Boom from '@hapi/boom';
+
+import dailyDevotionalService from '../../../services/customers/book/dailyDevotional.service'
+import { awsBucket } from '../../../constants/app.constant'
+import config from '../../../../config'
+import { responseMessage } from '../../../constants/message.constant'
+const NODE_ENV = config.NODE_ENV
+const readsOfDayControllerResponse = responseMessage.readsOfDayControllerResponse
+
+/** Get all author options list */
+const getOneDailyDevotional = async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const data = await dailyDevotionalService.getOneDailyDevotional({ _id: request.params.id })
+
+        if (!data) {
+            return next(
+                Boom.notFound(
+                    readsOfDayControllerResponse.getReadOfDayFailure
+                )
+            )
+        }
+
+        if (data.image) {
+            data.image = awsBucket[NODE_ENV].s3BaseURL + '/' + awsBucket.readsOfDayDirectory + '/' + data.image
+        }
+        response.status(200).json({ message: readsOfDayControllerResponse.fetchReadOfDaySuccess, data: data })
+    } catch (e: any) {
+        next(Boom.badData(e.message))
+    }
+}
+
+export { getOneDailyDevotional }
--- src/services/customers/subscriptions/subscriptions.service.ts
+++ src/services/customers/subscriptions/subscriptions.service.ts
@@ -1,16 +1,76 @@
 import { SubscriptionsModel } from '../../../models/index'
+import stripeSubscriptionService from '../../stripe/subscription';
 
 /** Get all Subscriptions for table */
 const getAllSubscriptions = async (search) => {
       try {
             const subscriptions: any = await SubscriptionsModel.find(search).lean().exec()
-            subscriptions.sort((a,b) => Number(a.price) - Number(b.price))
+            subscriptions.sort((a, b) => Number(a.price) - Number(b.price))
             return subscriptions
       } catch (e: any) {
             throw new Error(e)
       }
 }
 
+const getUserSubscriptionStatus = async (user) => {
+      try {
+            let subscriptionStatus;
+            let isPlanActive = false
+            let isPlanExpired = false
+            if (
+                  user.inAppSubscription &&
+                  [
+                        'active',
+                        'subscribed',
+                        'did_renew',
+                        'offer_redeemed'
+                  ].includes(
+                        user?.inAppSubscriptionStatus?.toLowerCase()
+                  )
+            ) {
+                  isPlanActive = true
+                  // todo: count duration with createdAt
+                  // if duration already ended
+                  // then mark plan as inactive
+            } else if (
+                  user.inAppSubscription &&
+                  ![
+                        'active',
+                        'subscribed',
+                        'did_renew',
+                        'offer_redeemed'
+                  ].includes(
+                        user?.inAppSubscriptionStatus?.toLowerCase()
+                  )
+            ) {
+                  isPlanExpired = true
+            }
+
+            if (!isPlanActive && user?.stripe?.subscriptionId) {
+                  try {
+                        const s = await stripeSubscriptionService
+                              .retrieveSubscription(
+                                    user.stripe.subscriptionId
+                              )
+                        isPlanActive = s?.status === 'active'
+                        isPlanExpired = !['active', 'trialing'].includes(s?.status?.toLowerCase())
+                  } catch (e) { }
+            }
+
+            if (!isPlanActive || isPlanExpired) {
+                  subscriptionStatus = "freemium"
+                  return subscriptionStatus
+            }
+            else {
+                  subscriptionStatus = "premium"
+                  return subscriptionStatus
+            }
+      } catch (e: any) {
+            throw new Error(e)
+      }
+}
+
 export default {
-      getAllSubscriptions
+      getAllSubscriptions,
+      getUserSubscriptionStatus
 }

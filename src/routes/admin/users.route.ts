817d67ea44e74b777e1024644c887507777ab543
Patoliya <developer.patoliya@gmail.com>
2024-08-06 16:00:08 +0530

    Feat : Add county filter and timezone filter

--- src/routes/admin/users.route.ts
+++ src/routes/admin/users.route.ts
@@ -5,12 +5,16 @@ import {
   deleteUser,
   getOneUser,
   getAllUsers,
+  getCountries,
+  getTimeZones
 } from '../../controllers/admin/users.controller'
 
 const router: Router = express.Router()
 
 router.post('/', addUser)
 router.get('/', getAllUsers)
+router.get('/countries', getCountries)
+router.get('/timezones', getTimeZones)
 router.get('/:userId', getOneUser)
 router.put('/:userId', updateUser)
 router.delete('/:userId', deleteUser)

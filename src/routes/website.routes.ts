b167beb72fb8c89ddebbf1a74612229173a63a30
Patoliya <developer.patoliya@gmail.com>
2024-11-18 15:10:32 +0530

    Feat : Subscribe user by email

--- src/routes/website.routes.ts
+++ src/routes/website.routes.ts
@@ -3,6 +3,7 @@ import express, { Router } from 'express'
 import testimonials from './website/testimonial.route'
 import subscriptions from './website/subscriptions.route'
 import bookCategories from './website/book/bookCategory.route'
+import subscriber from './website/subscriber.route'
 
 import cms from './website/cms.route'
 
@@ -13,5 +14,6 @@ router.use('/cms', cms)
 router.use('/categories', bookCategories)
 router.use('/testimonials', testimonials)
 router.use('/subscriptions', subscriptions)
+router.use('/subscribe', subscriber)
 
 export default router

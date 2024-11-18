8c3c0c5e764eac68bdb0cd717460fd1f2198ac1c
Patoliya <developer.patoliya@gmail.com>
2024-11-18 17:48:20 +0530

    Fix : Book summary feature

--- src/routes/website.routes.ts
+++ src/routes/website.routes.ts
@@ -3,6 +3,7 @@ import express, { Router } from 'express'
 import testimonials from './website/testimonial.route'
 import subscriptions from './website/subscriptions.route'
 import bookCategories from './website/book/bookCategory.route'
+import bookSummary from './website/book/bookSummary.route'
 import subscriber from './website/subscriber.route'
 import cms from './website/cms.route'
 
@@ -13,6 +14,7 @@ router.use('/cms', cms)
 router.use('/categories', bookCategories)
 router.use('/testimonials', testimonials)
 router.use('/subscriptions', subscriptions)
+router.use('/book-summary', bookSummary)
 router.use('/subscribe', subscriber)
 
 export default router

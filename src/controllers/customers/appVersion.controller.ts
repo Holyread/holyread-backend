56664dbc292dbf4501a323eb30ee765e317d76b9
Vijay P <osi.vijay@gmail.com>
2025-08-12 15:49:16 +0530

    feat- add validation check for platform

--- src/controllers/customers/appVersion.controller.ts
+++ src/controllers/customers/appVersion.controller.ts
@@ -3,19 +3,35 @@ import Boom from '@hapi/boom';
 
 import appVersionService from '../../services/customers/appVersion/appVersion.service'
 
-const checkUserAppVersion = async (request: Request, response: Response, next: NextFunction) => {
-    try {
-        const data = await appVersionService.getOneAppVersionByFilter({ platform: request.body.platform });
-
-        let shouldUpdateAppVersion: boolean = true;
-
-        if (data.version === request.body.version) {
-            shouldUpdateAppVersion = false;
-        }
-
-        response.status(200).json({ shouldUpdateAppVersion , version: data.version });
-    } catch (e: any) {
-        next(Boom.badData(e.message));
+const checkUserAppVersion = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const { platform, version } = request.body;
+    
+    const allowedPlatforms = ["android", "ios"];
+    if (!platform || !allowedPlatforms.includes(platform.toLowerCase())) {
+      return next(
+        Boom.badRequest("Invalid platform. Only android and ios are supported.")
+      );
     }
+    const data = await appVersionService.getOneAppVersionByFilter({
+      platform: request.body.platform,
+    });
+
+    let shouldUpdateAppVersion: boolean = true;
+
+    if (data.version === version) {
+      shouldUpdateAppVersion = false;
+    }
+
+    response
+      .status(200)
+      .json({ shouldUpdateAppVersion, version: data.version });
+  } catch (e: any) {
+    next(Boom.badData(e.message));
+  }
 };
 export { checkUserAppVersion }

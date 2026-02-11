8751366b210fb8b8a8b8940ae67ce24068705603
Vijay P <osi.vijay@gmail.com>
2026-02-11 17:08:43 +0530

    fix: Update language handling in CMS and book category controllers

--- src/routes/website.routes.ts
+++ src/routes/website.routes.ts
@@ -6,8 +6,11 @@ import bookCategories from './website/book/bookCategory.route'
 import bookSummary from './website/book/bookSummary.route'
 import subscriber from './website/subscriber.route'
 import cms from './website/cms.route'
+import { languageMiddleware } from '../middleware/language.middleware'
 
 const router: Router = express.Router()
+
+router.use(languageMiddleware);
 
 /** Routes for website */
 router.use('/cms', cms)
